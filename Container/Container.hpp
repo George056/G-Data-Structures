c5b47c4f03fc9cc3b4da94f800c5353207b1f52c
George056 <70665330+George056@users.noreply.github.com>
2021-01-09 09:36:49 -0600

    Container updated

--- Container/Container.hpp
+++ Container/Container.hpp
@@ -8,34 +8,41 @@ This is an abstract class used as a base for all container types
 
 namespace george_data_structures {
 
-		template <typename dataType, class iterator_type>
+		template <typename dataType, class iterator_type, class const_iterator_type = input_iterator<dataType>>
 		class container {
-		protected:
-				size_t __size;
-
+		public:
+				using size_type = size_t;
 				using iterator = iterator_type;
+				using const_iterator = const_iterator_type;
+
+		protected:
+				size_type __size;
+				inline void set_size(size_type i) { __size = i; }
+				inline size_type get_size() { return __size; }
 
 		public:
+
 				container() : __size{ 0 } {}
-				virtual container(const container&) = 0;
+				container(size_type size) : __size{ size } {}
+				container(const container&) = default;
 				container(container&&) = default;
 
 				virtual ~container() = default;
 
-				virtual container& operator =(const container&) = 0;
-				virtual container& operator =(container&&) = 0;
-				virtual dataType& operator [](size_t index) = 0;
+				virtual container& operator =(const container&) = default;
+				virtual container& operator =(container&&) = default;
+				virtual dataType& operator [](size_type index) = 0;
 
 				virtual dataType& at(size_t index) = 0;
 
-				virtual size_t size() { return __size; }
-				virtual bool empty() { return __size == 0; }
+				inline virtual size_type size() const { return __size; }
+				inline virtual bool empty() const { return __size == 0; }
 
 				virtual iterator begin() = 0;
 				virtual iterator end() = 0;
 
-				virtual output_iterator cbegin() = 0;
-				virtual output_iterator cend() = 0;
+				virtual const_iterator cbegin() const = 0;
+				virtual const_iterator cend() const = 0;
 
 		};
 
